7c8ef6ad6b14c475dec9b2fb68158ccd68d72f0b
mef231 <mef231@gmail.com>
2016-05-19 13:56:41 -0400

    Pcff typing debugging

--- apps/random_walk.py
+++ apps/random_walk.py
@@ -292,9 +292,9 @@ def random_walk(m, nmon, s_=None, **kwargs):
         backbone_vector = np.array(find_last_backbone_vector(s, m))
 
         for p, p_ in izip(s.particles[-1*m.particles.count:], m.particles):
-                p_.x = p.x + 2*backbone_vector[0]
-                p_.y = p.y + 2*backbone_vector[1]
-                p_.z = p.z + 2*backbone_vector[2]
+                p_.x = p.x + 5*backbone_vector[0]
+                p_.y = p.y + 5*backbone_vector[1]
+                p_.z = p.z + 5*backbone_vector[2]
 
         n = m.copy()
 
