c3f4f0fd37eba90bd77814c927bc9e93c1f3d0bd
mef231 <mef231@gmail.com>
2016-06-07 15:39:10 -0400

    random_walk debugging

--- apps/random_walk.py
+++ apps/random_walk.py
@@ -349,9 +349,8 @@ def random_walk(m, nmon, s_=None, **kwargs):
             sim = lmps.Simulation(s, name='relax_%03d' % (insertion+2), log='relax.log', **settings)
             sim.add_md(ensemble='nve', limit=limit, **settings)
             sim.add_min(**settings)
-        elif isinstance(sim, lmps.Simulation):
-            sim.system = s
         if isinstance(sim, lmps.Simulation):
+            sim.system = s
             sim.run(np=settings.get('np'))
 
         if unwrap:
