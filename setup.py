4d26701bece398761cb3d678fcd272e6a0ffdd70
mefortunato <mef231@gmail.com>
2016-07-01 11:18:38 -0400

    restructured

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysimm',
+    version='0.1',
+    description='Python Simulation Interface for Molecular Modeling',
+    url='http://github.com/polysimtools/pysimm',
+    author='Michael Fortunato',
+    author_email='mefortunato@pysimm.org',
+    license='MIT',
+    packages=['pysimm'],
+    zip_safe=False)
