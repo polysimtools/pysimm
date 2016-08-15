6162ea7799f837bfb7de44bd7525ff480303011e
Michael Fortunato <mef231@gmail.com>
2016-08-15 15:57:25 +0000

    add coray as author

--- pysimm/apps/random_walk.py
+++ pysimm/apps/random_walk.py
@@ -9,7 +9,7 @@
 # ******************************************************************************
 # The MIT License (MIT)
 #
-# Copyright (c) 2016 Michael E. Fortunato
+# Copyright (c) 2016 Michael E. Fortunato, Coray M. Colina
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
