0ea8b1cc4265cf4b346303d9e935ecccfd9bc616
Michael Fortunato <mef231@gmail.com>
2016-11-29 20:23:46 +0000

    added dreiding examples in addition to gaff2 examples

---
+++ Examples/dreiding/01_methane/create.py
@@ -0,0 +1,54 @@
+from pysimm import system, lmps, forcefield
+
+# create empty system
+s = system.System()
+
+# create new molecule in our system
+m = s.molecules.add(system.Molecule())
+
+# retrieve Dreiding parameters
+f = forcefield.Dreiding()
+
+# get a copy of the c3 particle type object from GAFF
+# get method returns a list, we need the first element
+dreiding_C_ = s.particle_types.add(f.particle_types.get('C_3')[0].copy())
+
+# we'll first make the carbon atom at the origin
+# we'll include gasteiger charges later
+c1 = s.particles.add(system.Particle(type=dreiding_C_, x=0, y=0, z=0, charge=0, molecule=m))
+
+# get hc particle type object from GAFF
+dreiding_H_ = s.particle_types.add(f.particle_types.get('H_')[0].copy())
+
+# now we'll add 4 hydrogen atoms bonded to our carbon atom
+# these atoms will be placed randomly 1.5 angstroms from the carbon atom
+# we'll optimize the structure using LAMMPS afterwords
+# we supply the GAFF forcefield object so that bond and angle types can be added as well
+h1 = s.add_particle_bonded_to(system.Particle(type=dreiding_H_, charge=0, molecule=m), c1, f)
+h2 = s.add_particle_bonded_to(system.Particle(type=dreiding_H_, charge=0, molecule=m), c1, f)
+h3 = s.add_particle_bonded_to(system.Particle(type=dreiding_H_, charge=0, molecule=m), c1, f)
+h4 = s.add_particle_bonded_to(system.Particle(type=dreiding_H_, charge=0, molecule=m), c1, f)
+
+# let's add gasteiger charges
+s.apply_charges(f, charges='gasteiger')
+
+# right now there is no simulation box defined
+# we'll define a box surrounding our methane molecule with a 10 angstrom padding
+s.set_box(padding=10)
+
+# before we optimize our structure, LAMMPS needs to know what type of 
+# pair, bond, and angle interactions we are using
+# these are determined by the forcefield being used
+s.pair_style='lj'
+s.bond_style='harmonic'
+s.angle_style='harmonic'
+
+# we'll perform energy minimization using the fire algorithm in LAMMPS
+lmps.quick_min(s, min_style='sd', name='sd')
+lmps.quick_min(s, min_style='cg', name='cg')
+
+# write xyz, YAML, LAMMPS data, and chemdoodle json files
+s.write_xyz('methane.xyz')
+s.write_yaml('methane.yaml')
+s.write_lammps('methane.lmps')
+s.write_chemdoodle_json('methane.json')
---
+++ Examples/dreiding/06_polymethyl_methacryalte_multi-chain/create.py
@@ -0,0 +1,37 @@
+from pysimm import system, lmps, forcefield
+from pysimm.apps.random_walk import random_walk
+
+# use a smiles string to query the pubchem search database and read the mol file returned from the http request
+pmma = system.read_pubchem_smiles('cc(C)(C(=O)OC)')
+
+# we'll instantiate a Dreiding forcefield object for use later
+f = forcefield.Dreiding()
+
+# particles 3 and 6 in the monomer are going to be the head and tail linkers
+pmma.particles[3].linker='head'
+pmma.particles[6].linker='tail'
+
+# the resulting system has sufficient information to type with the forcefield object we made earlier
+# we will also determine partial charges using the gasteiger algorithm
+pmma.apply_forcefield(f, charges='gasteiger')
+
+# do a quick minimization of the monomer
+lmps.quick_min(pmma, min_style='fire')
+
+# write a yaml file for the pmma monomer
+pmma.write_yaml('pmma_monomer.yaml')
+
+pmma.pair_style = 'lj'
+
+# we're going to make 4 chains, each of 5 repeat units
+# the first system we make will be used as the initial system for the subsequent random walk calls
+polymer = random_walk(pmma , nmon=5, forcefield=f, density=0.3/4)
+polymer = random_walk(pmma , nmon=5, s_=polymer, forcefield=f)
+polymer = random_walk(pmma , nmon=5, s_=polymer, forcefield=f)
+polymer = random_walk(pmma , nmon=5, s_=polymer, forcefield=f)
+
+# write a few different file formats
+polymer.write_xyz('polymer.xyz')
+polymer.write_yaml('polymer.yaml')
+polymer.write_lammps('polymer.lmps')
+polymer.write_chemdoodle_json('polymer.json')
