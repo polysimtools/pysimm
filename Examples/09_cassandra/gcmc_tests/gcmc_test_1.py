c0b7dc4cd78414a545cc45e825d1bfa14e1c3af2
AlleksD <alleks.g.d@ufl.edu>
2017-07-05 12:55:55 -0400

    Updated the Test1 -- insertion of H2 to the empty box (with no PIM in it)

--- Examples/09_cassandra/gcmc_tests/gcmc_test_1.py
+++ Examples/09_cassandra/gcmc_tests/gcmc_test_1.py
@@ -1,27 +1,27 @@
 from pysimm import system, cassandra
+from collections import OrderedDict
 
-# Create blank system object (It can be formed in many ways here we basically need
-#   a box with a certain size to populate it afterwards with gas molecules
-sst = system.System()
-bxSize = 100
-sst.dim.dx = bxSize
-sst.dim.dy = bxSize
-sst.dim.dz = bxSize
+bxSize = 44.2325
 
 # In order to run CASSANDRA GCMC one need to create the CASSANDRA object
-css = cassandra.Cassandra(sst)
+css = cassandra.Cassandra()
 
 # Read the some default CASSANDRA .inp parameters file
-gcmcPropsRead = css.read_input('default.inp')
-
-# Set the gas to be purged in a box
-gcmcPropsRead['adsorbers'] = [['ch4', 2000]]
-
-gcmcPropsRead['Chemical_Potential_Info'] = -33.640934
+gcmcPropsRead = css.read_input('my_props.inp')
+gcmcPropsRead['Box_Info'] = OrderedDict([('box_count', 1), ('box_size', bxSize)])
 
 # The prefix for the all files that will be created by this run
-gcmcPropsRead['Run_Name'] = 'gcmc_test_1'
-css.add_gcmc(**gcmcPropsRead)
+gcmcPropsRead['Run_Name'] = 'no_pim_box'
+gcmcPropsRead['Rcutoff_Low'] = 0.
+
+# Set the gas (gas system) to be purged in a box
+h2 = system.read_lammps('h2.lmps')
+gas_syst = cassandra.McSystem(h2, max_ins=5000, chem_pot=-23.34)
+
+job = cassandra.GCMC(gas_syst, out_folder='gcmc_test_1', **gcmcPropsRead)
+css.add_gcmc(job)
+
 css.run()
 
-# css.system.visualize()
+job.tot_sst.write_lammps('hydrogens.lmps')
+job.tot_sst.system.visualize()
