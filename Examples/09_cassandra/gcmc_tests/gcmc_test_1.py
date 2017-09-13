43f167f6dd0caeb320020cd077dbf45c4cbfb7af
AlleksD <alleks.g.d@ufl.edu>
2017-09-13 18:13:46 -0400

    Refactor of the MC-MD restart script plus small improvements in example input files

--- Examples/09_cassandra/gcmc_tests/gcmc_test_1.py
+++ Examples/09_cassandra/gcmc_tests/gcmc_test_1.py
@@ -1,25 +1,23 @@
 from pysimm import system, cassandra
 from collections import OrderedDict
 
-bxSize = 44.2325
-
 # In order to run CASSANDRA GCMC one need to create the CASSANDRA object
 css = cassandra.Cassandra()
 
 # Read the some default CASSANDRA .inp parameters file
-gcmcPropsRead = css.read_input('my_props.inp')
-gcmcPropsRead['Box_Info'] = OrderedDict([('box_count', 1), ('box_size', bxSize)])
+gcmcPropsRead = css.read_input('my_props_simple.inp')
+gcmcPropsRead['Box_Info'] = OrderedDict([('box_count', 1), ('box_size', 45)])
 
 # The prefix for the all files that will be created by this run
-gcmcPropsRead['Run_Name'] = 'no_pim_nitrogens'
+gcmcPropsRead['Run_Name'] = 'pure_co2'
 gcmcPropsRead['Rcutoff_Low'] = 0.
 
 # Set the gas (gas system) to be purged in a box
-gas_syst = cassandra.McSystem(system.read_lammps('n2.lmps'), max_ins=5000, chem_pot=-27.34)
+gas_sst = cassandra.McSystem(system.read_lammps('co2.lmps'), max_ins=2000, chem_pot=-27.34)
 
-job = cassandra.GCMC(gas_syst, out_folder='gcmc_test_1', **gcmcPropsRead)
+job = cassandra.GCMC(gas_sst, out_folder='gcmc_test_1', **gcmcPropsRead)
 css.add_gcmc(job)
-
 css.run()
+
 job.tot_sst.write_lammps('hydrogens.lmps')
 # job.tot_sst.system.visualize()
--- Examples/09_cassandra/gcmc_tests/mcmd_restart.py
+++ Examples/09_cassandra/gcmc_tests/mcmd_restart.py
@@ -1,30 +1,21 @@
-from pysimm import system, lmps, cassandra
-from collections import OrderedDict
 import os
 import re
-import random
+from collections import OrderedDict
+from pysimm import system, lmps, cassandra
+import glob
 
 MAX_INS = 2000
-CHEM_POT = -33.15
+CHEM_POT = -36.7264
 TMPR = 300
-L_MAX = 11
+L_MAX = 21
+continue_simulations = True
 
-# Restarting parameters
-restart_ind = 11
+xyz_fname = 'MD{:}_out.xyz'
 
 # First, for CASSANDRA GCMC the governing CASSANDRA object should be created
 css = cassandra.Cassandra()
 # Read the some pre-prepared CASSANDRA .inp parameters file that can be modified then programatically
 gcmcPropsRead = css.read_input('my_props.inp')
-# Creating fixed polymer system
-fixed_sst = system.read_lammps('pim.lmps')
-# Reading "reastart dump" that is simply the .lmps file from one of the previous MC-MD iterations
-fixed_ref_sst = system.read_lammps(os.path.join('results', '10.md.lammps'))
-for p in fixed_sst.particles:
-    p.x = fixed_ref_sst.particles[p.tag].x
-    p.y = fixed_ref_sst.particles[p.tag].y
-    p.z = fixed_ref_sst.particles[p.tag].z
-fixed_sst.dim = fixed_ref_sst.dim
 
 # Setting the gas system
 co2 = system.read_lammps('co2.lmps')
@@ -32,20 +23,49 @@ co2 = system.read_lammps('co2.lmps')
 # I Reading and arraging data from previous MC step
 gas_mc = cassandra.McSystem(co2, chem_pot=CHEM_POT, max_ins=MAX_INS, is_rigid=True)
 out_folder = 'results'
-gcmcPropsRead['Run_Name'] = str(restart_ind) + '.gcmc'
-gcmc = cassandra.GCMC(gas_mc, fixed_sst, out_folder=out_folder, props_file=str(restart_ind) + '.gcmc_props.inp',
-                      **gcmcPropsRead)
-gcmc.upd_simulation()
 
-l = restart_ind  # loop variable
+# Creating fixed polymer system
+fixed_sst = system.read_lammps('pim.lmps')
+fixed_sst.wrap()
+l = 1
+
+# This is used to continue simulations using the most fresh files taken from the SAME out_folder
+if continue_simulations:
+    # Figure out what is the last iteration with finished CASSANDRA
+    da = cassandra.DataAnalyzer(path=out_folder, mc_fname_mask='*.before_md.lmps')
+    l = int(re.search('\A\d+', os.path.split(da.file_names[-1])[1]).group())
+    fixed_ref_sst = system.read_lammps(os.path.join(da.file_names[-1]))
+
+    # Clearing previous LAMMPS log file if it exists
+    prev_log = os.path.join(out_folder, str(l) + '.md.log')
+    if os.path.exists(prev_log):
+        os.remove(prev_log)
+
+    # Updating simulation system with the data from the latest lmps file
+    for p in fixed_sst.particles:
+        p.x = fixed_ref_sst.particles[p.tag].x
+        p.y = fixed_ref_sst.particles[p.tag].y
+        p.z = fixed_ref_sst.particles[p.tag].z
+    fixed_sst.dim = fixed_ref_sst.dim
+
+gcmcPropsRead['Run_Name'] = str(l) + '.gcmc'
+gcmc = cassandra.GCMC(gas_mc, fixed_sst, out_folder=out_folder, props_file=str(l) + '.gcmc_props.inp', **gcmcPropsRead)
+css.add_gcmc(gcmc)
+
+if continue_simulations:
+    gcmc.upd_simulation()
+else:
+    css.run()
+
 time_step = 0.5
-lngth = 2e+6
-nonrig_group_name = 'nonrigid_b'
-rig_group_name = 'rigid_b'
-while l < L_MAX + restart_ind:
+lngth = 2e+4
+nonrig_group_name = 'nonrig_bod'
+rig_group_name = 'rig_bod'
+while l < L_MAX:
     # >>> 2N: MD (LAMMPS) step:
-    sim = lmps.Simulation(gcmc.tot_sst, log=os.path.join(out_folder, str(l) + '.md.log'),
-                          print_to_screen=True, cutoff=14.0)
+    sim_sst = gcmc.tot_sst
+    sim_sst.write_lammps(os.path.join(out_folder, str(l) + '.before_md.lmps'))
+    sim = lmps.Simulation(sim_sst, log=os.path.join(out_folder, str(l) + '.md.log'), cutoff=14.0)
 
     # custom definitions for the neighbour list updates
     sim.add_custom('neighbor 1.0 nsq \nneigh_modify once no every 1 delay 0 check yes')
@@ -56,9 +76,8 @@ while l < L_MAX + restart_ind:
     sim.add_custom(grp_tmpl.format(rig_group_name, gcmc.group_by_id('rigid')[0]))
 
     # create the description of the molecular dynamics simulation
-    tmp_md = lmps.MolecularDynamics(ensemble='npt', timestep=time_step, length=int(lngth), thermo=2500, pressure=20,
-                                    temp=TMPR, dump=5000, dump_name=os.path.join(out_folder, str(l) + '.md.dump'),
-                                    new_v=True, scale_v=True)
+    tmp_md = lmps.MolecularDynamics(ensemble='npt', timestep=time_step, length=int(lngth), thermo=2500, pressure=5,
+                                    temp=TMPR, new_v=True, scale_v=True)
 
     # obtain the simulation (LAMMPS) input in order to customly modify it later
     tmp_md.write(sim)
@@ -82,13 +101,17 @@ while l < L_MAX + restart_ind:
 
     # The input for correct simulations is set, starting LAMMPS:
     sim.add_custom(tmp_md.input)
-    sim.run()
+    sim.run(np=4)
 
     # Updating the size of the fixed system from the MD simulations for the next MC
     fixed_sst.dim = sim.system.dim
 
     # >>> 2N + 1: MC (CASSANDRA) step
-    new_xyz_file = 'MD' + str(l) + '_out.xyz'
+    new_xyz_file = xyz_fname.format(str(l))
+
+    # right after the MD the new .xyz file to read start configuration from is saved
+    sim.system.write_xyz(os.path.join(out_folder, new_xyz_file))
+
     gcmcPropsRead['Run_Name'] = str(l + 1) + '.gcmc'
 
     # The 'Start_Type' property should be changed in order to make CASSANDRA read previously simulated system
@@ -99,9 +122,6 @@ while l < L_MAX + restart_ind:
     gas_mc = cassandra.McSystem(co2, chem_pot=CHEM_POT, max_ins=MAX_INS, is_rigid=True)
     gcmc = cassandra.GCMC(gas_mc, fixed_sst, out_folder=out_folder,
                           props_file=str(l + 1) + '.gcmc_props.inp', **gcmcPropsRead)
-    # now we make new .xyz file to read start configuration from
-    sim.system.write_xyz(os.path.join(out_folder, new_xyz_file))
     css.add_gcmc(gcmc, is_new=True)
     css.run()
-    sim_sst = gcmc.tot_sst
     l += 1
