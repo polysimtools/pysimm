6cfdb94aa9efa6ec0a6a476e604a8c971997c474
Michael E. Fortunato <mef231@gmail.com>
2017-09-21 08:46:20 -0400

    added first tests

--- Examples/05_polyethylene-co-styrene/gaff2/create.py
+++ Examples/05_polyethylene-co-styrene/gaff2/create.py
@@ -3,22 +3,26 @@ from pysimm.apps.random_walk import copolymer
 from pysimm.models.monomers.gaff2.pe import monomer as pe_monomer
 from pysimm.models.monomers.gaff2.ps import monomer as ps_monomer
 
-# we'll make a polyethylene monomer and a polystyrene monomer from the pysimm models database
-pe = pe_monomer()
-ps = ps_monomer()
-
-# we'll instantiate a GAFF2 forcefield object for use later
-f = forcefield.Gaff2()
-
-# the monomers do not have any charges, so we will derive partial charges using the gasteiger algorithm
-pe.apply_charges(f, charges='gasteiger')
-ps.apply_charges(f, charges='gasteiger')
-
-# run the copolymer random walk method with 10 total repeat units, using an alternating pattern
-polymer = copolymer([pe, ps], 10, pattern=[1, 1], forcefield=f)
-
-# write a few different file formats
-polymer.write_xyz('polymer.xyz')
-polymer.write_yaml('polymer.yaml')
-polymer.write_lammps('polymer.lmps')
-polymer.write_chemdoodle_json('polymer.json')
+def run(test=False):
+    # we'll make a polyethylene monomer and a polystyrene monomer from the pysimm models database
+    pe = pe_monomer()
+    ps = ps_monomer()
+    
+    # we'll instantiate a GAFF2 forcefield object for use later
+    f = forcefield.Gaff2()
+    
+    # the monomers do not have any charges, so we will derive partial charges using the gasteiger algorithm
+    pe.apply_charges(f, charges='gasteiger')
+    ps.apply_charges(f, charges='gasteiger')
+    
+    # run the copolymer random walk method with 10 total repeat units, using an alternating pattern
+    polymer = copolymer([pe, ps], 10, pattern=[1, 1], forcefield=f)
+    
+    # write a few different file formats
+    polymer.write_xyz('polymer.xyz')
+    polymer.write_yaml('polymer.yaml')
+    polymer.write_lammps('polymer.lmps')
+    polymer.write_chemdoodle_json('polymer.json')
+    
+if __name__ == '__main__':
+    run()
