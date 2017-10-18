36dd6ad6d9196ea3c0c862e701d02f024cf9726a
AlleksD <alleks.g.d@ufl.edu>
2017-10-18 15:16:21 -0400

    Small simulation parameters adjustement in example-10

--- Examples/10_mof_swelling/run.py
+++ Examples/10_mof_swelling/run.py
@@ -1,10 +1,8 @@
 from pysimm.apps import mc_md
-from pysimm import system, lmps, cassandra
+from pysimm import system
 
 polymer = system.read_lammps('irmof1_drei.lmps', dihedral_style='fourier')
 gas1 = system.read_lammps('ch4.lmps')
-
-cs = cassandra.Cassandra(polymer)
 
 mc_props = {'rigid_type': False,
             'max_ins': 1000,
@@ -12,9 +10,9 @@ mc_props = {'rigid_type': False,
             'Temperature_Info': 300,
             'Run_Type': {'steps': 250},
             'CBMC_Info': {'rcut_cbmc': 2.0},
-            'Simulation_Length_Info': {'run': 100000,
-                                       'coord_freq': 100000,
-                                       'prop_freq': 1000},
+            'Simulation_Length_Info': {'run': 10000,
+                                       'coord_freq': 10000,
+                                       'prop_freq': 500},
             'VDW_Style': {'cut_val': 9.0},
             'Charge_Style': {'cut_val': 9.0},
             'Property_Info': {'prop1': 'energy_total',
@@ -23,13 +21,12 @@ mc_props = {'rigid_type': False,
 md_props = {'ensemble': 'npt',
             'temp': 300,
             'pressure': 15,
-            'timestep': 0.3,
+            'timestep': 0.25,
             'cutoff': 9.0,
-            'length': 100000,
+            'length': 10000,
             'thermo': 2500,
             'dump': 2500,
             'print_to_screen': False}
 
 sim_result = mc_md.mc_md(gas1, polymer, mcmd_niter=3, sim_folder='results', mc_props=mc_props, md_props=md_props)
 sim_result.write_lammps('MOFplusME.lmps')
-
