a21357e7bd910e7c51dc5b96a534a66a2c845872
AlleksD <alleks.g.d@ufl.edu>
2018-01-12 18:25:14 -0500

    Update of Example 10 to match the way it is described in the text: MOF-5 is replaced with IRMOF-14, and the MOF preparation file is added

--- Examples/10_mof_swelling/run.py
+++ Examples/10_mof_swelling/run.py
@@ -4,37 +4,38 @@ from pysimm import system
 
 
 def run(test=False):
-    frame = system.read_lammps(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'irmof1_drei.lmps'))
+    frame = system.read_lammps(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'irmof-14.lmps'))
     frame.forcefield = 'dreiding-lj'
     gas1 = system.read_lammps(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ch4.lmps'))
     gas1.forcefield = 'trappe/amber'
     
     mc_props = {'rigid_type': False,
-                'max_ins': 1000,
-                'Chemical_Potential_Info': -30.09,
+                'max_ins': 2000,
+                'Chemical_Potential_Info': -22.5037,
                 'Temperature_Info': 300,
-                'Run_Type': {'steps': 250},
+                'Run_Type': {'steps': 100},
                 'CBMC_Info': {'rcut_cbmc': 2.0},
-                'Simulation_Length_Info': {'run': 100000,
-                                           'coord_freq': 100000,
-                                           'prop_freq': 500},
-                'VDW_Style': {'cut_val': 9.0},
-                'Charge_Style': {'cut_val': 9.0},
+                'Simulation_Length_Info': {'run': 300000,
+                                           'coord_freq': 300000,
+                                           'prop_freq': 1000},
+                'VDW_Style': {'cut_val': 14.0},
+                'Charge_Style': {'cut_val': 14.0},
                 'Property_Info': {'prop1': 'energy_total',
                                   'prop2': 'pressure',
                                   'prop3': 'nmols'}}
     
     md_props = {'temp': 300,
-                'pressure': {'start': 15,
+                'pressure': {'start': 300,
                              'iso': 'iso'},
                 'timestep': 1,
-                'cutoff': 9.0,
-                'length': 50000,
+                'cutoff': 14.0,
+                'length': 100000,
                 'thermo': 1000,
                 'dump': 1000,
+                'np': 8,
                 'print_to_screen': False}
     
-    sim_result = mc_md.mc_md(gas1, frame, mcmd_niter=3, sim_folder='results', mc_props=mc_props, md_props=md_props)
+    sim_result = mc_md.mc_md(gas1, frame, mcmd_niter=5, sim_folder='results', mc_props=mc_props, md_props=md_props)
     sim_result.write_lammps('MOFplusME.lmps')
     sim_result.write_xyz('MOFplusME.xyz')
 
