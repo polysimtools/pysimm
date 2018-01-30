fed4d57b6068231b2116138f913f224ed8cad129
AlleksD <alleks.g.d@ufl.edu>
2018-01-30 16:14:19 -0500

    Example 12 modifications (now results are comparable with the experiment)

--- Examples/12_acetelene_npt/run.py
+++ Examples/12_acetelene_npt/run.py
@@ -4,27 +4,21 @@ from pysimm import system, cassandra, lmps
 # Setup the box with acetelene molecules on the regular grid
 sst = system.System()
 
-bx_size = 30
+bx_size = 60
 sst.dim = system.Dimension(dx=bx_size, dy=bx_size, dz=bx_size, center=[bx_size / 2, bx_size / 2, bx_size / 2])
 sst.forcefield = 'trappe/amber'
 
 molec = system.read_lammps('c2h4.lmps')
 molec.forcefield = 'trappe/amber'
 
-# Creating system of 1000 molecules for NPT calculations
-# displ = 3
-# for i in range(10):
-#     for j in range(10):
-#         for k in range(10):
-#             sst.add(molec.copy(dx=displ*i, dy=displ*j, dz=displ*k), change_dim=False)
-#
-# lmps.check_lmps_attr(sst)
-# sst.write_lammps('init_conf.lmps')
-
 cs = cassandra.Cassandra(sst)
 npt_props = cs.read_input('props.inp')
 npt_props['Pressure_Info'] = 25  # Simulated pressure in bars
-npt_props['Start_Type'] = {'start_type': 'make_config', 'species': 1000}
+npt_props['Start_Type'] = {'start_type': 'make_config', 'species': 300}
+npt_props['Simulation_Length_Info'] = {'run': 500000}
+npt_props['Property_Info'] = {'prop1': 'energy_total',
+                              'prop2': 'volume',
+                              'prop3': 'mass_density'}
 
 cs.add_npt_mc(species=molec, is_rigid=True, out_folder='results', **npt_props)
 
