84022dc0b80d4eb093b88695012c0df770b5fde9
AlleksD <AlleksD@gmail.com>
2020-05-01 18:14:27 -0400

    correct version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Mike Fortunato'
 author = 'Mike Fortunato'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
